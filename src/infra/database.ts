5364041f2947a429806429fe6df902a0d9576f9a
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-06-17 16:09:16 -0300

    feat: configure dotenv for environment variable management and update database connection settings

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,6 +1,19 @@
 import { PrismaClient } from '../generated/prisma/client.js';
+import dotenv from 'dotenv';
+import path from 'path';
+import dotenvExpand from 'dotenv-expand';
+
+const env = process.env.NODE_ENV || 'development';
+dotenv.config({ path: path.resolve(process.cwd(), `.env.${env}`) });
+dotenvExpand.expand(
+  env
+    ? dotenv.config({ path: path.resolve(process.cwd(), `.env.${env}`) })
+    : {},
+);
 
 const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+  errorFormat: 'minimal',
   datasources: {
     db: {
       url: process.env.DATABASE_URL,
